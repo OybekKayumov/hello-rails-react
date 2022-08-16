d380bd4b6e77947ed54ee252030292a689360c13
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-16 05:58:24 +0500

    add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    root 'root#index'
+  end
 end
