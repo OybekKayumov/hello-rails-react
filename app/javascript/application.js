153e8950c2a88043c226628f641819a77d7a8a84
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-08-17 06:25:52 +0500

    update Procfile

---
+++ app/javascript/application.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root'),
+// );
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
